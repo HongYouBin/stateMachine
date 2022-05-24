bd9c9e84c3d20b7cbb37bb13e72d2a143817e4cf
HongYouBin <ghddbqls33@gmail.com>
2022-05-24 17:29:32 +0900

    state machine

---
+++ trip/StateMachine.cpp
@@ -0,0 +1,141 @@
+﻿#include <string>
+#include "StateMachine.h"
+
+
+/*
+	함수 이름 : StateMachine::run()
+	기능	  : STATE_END 상태가 될 때까지 event-action 테이블에 정의된 규칙과 사용자에 의해 발생되는 이벤트에 따라 state machine을 동작시킴
+	전달 인자 : 없음
+	반환값    : 없음
+*/
+void StateMachine::run()
+{
+	int i, j;
+	Event curEvent;		// 현재 이벤트
+
+	curState = READY;				// 상태 초기화
+    while (true)		
+	{ 
+		curEvent = getNextEvent();		// 다음 이벤트를 입력받음
+		
+        for (i = 0; i < NUMBER_OF_TABLE_ENTRIES; i++)   // 테이블에 정의된 전각체 행(transition 내용)에 대해 비교함
+		{ 
+			if (curState == table[i].curState)			// 현재 상태와 일치하는지 검사
+			{ 
+				if (curEvent == table[i].event)			// 입력된 현재 이벤트와 일치하는지 검사
+				{
+					// 해당 transition이 발생할 때 수행해야 할 action 함수들을 실행시킴
+				(this->*table[i].action)();
+				curState = table[i].nextState;		// 테이블에 정의된 다음 상태로 현재 상태를 변경함
+				break;
+				}
+			} 
+		} 
+		cout << endl << endl;
+	}
+}
+
+
+
+void StateMachine::tripRequested()
+{
+	cout << "request trip" << '\n';
+}
+
+void StateMachine::paymentFailed()
+{
+	cout << "payment failed" << '\n';
+}
+
+void StateMachine::requestPayment()
+{
+	cout << "request payment" << '\n';
+}
+
+void StateMachine::paymentSucceeded()
+{
+	cout << "payment succeeded" << '\n';
+}
+
+void StateMachine::tripCompleted()
+{
+	cout << "trip completed" << '\n';
+}
+
+/*
+	함수 이름 : StateMachine::getNextEvent()
+	기능	  : 사용자로부터 다음 이벤트를 입력받음
+	전달 인자 : 없음
+	반환값    : Event -> 입력받은 이벤트 값
+*/
+Event StateMachine::getNextEvent()
+{
+	Event selectedEvent;       // 반환할 이벤트
+	int inputEvent;				// 사용자로부터 입력받은 이벤트
+
+	// 입력 안내 메세지 출력
+	cout << "[현재 상태 : " << getCurrentStateString() << "]" << endl;
+	cout << "1. Press request trip" << endl;
+	cout << "2. Press request payment" << endl;
+	cout << "3. Press payment succeeded" << endl;
+	cout << "4. Press payment failed" << endl;
+	cout << "5. Press trip-completed" << endl;
+
+	cout << "Select an event # : ";
+
+	// 사용자로부터 이벤트 입력받음
+	cin >> inputEvent;
+
+	// 입력 값에 따라 반환할 이벤트 값을 결정함
+	switch (inputEvent)
+	{
+	case 1:
+		selectedEvent = REQUEST_TRIP;
+		break;
+	case 2:
+		selectedEvent = REQUEST_PAYMENT;
+		break;
+	case 3:
+		selectedEvent = PAYMENT_SUCCEEDED;
+		break;
+	case 4:		
+		selectedEvent = PAYMENT_FAILED;
+		break;
+	case 5:
+		selectedEvent = TRIP_COMPLETED;
+		break;
+	
+	}
+
+	return selectedEvent;
+}
+
+
+
+/*
+	함수 이름 : StateMachine::getCurrentStateString()
+	기능	  : 현재 상태에 해당하는 출력용 문자열을 생성해서 반환함
+	전달 인자 : 없음
+	반환값    : string -> 출력용 상태 문자열
+*/
+string StateMachine::getCurrentStateString()
+{
+	string returnedString;
+
+	switch (curState)
+	{
+	case READY:
+		returnedString = "STATE_READY";
+		break;
+	case TRIP_REQUESTED:
+		returnedString = "STATE_TRIP REQUESTED";
+		break;
+	case PAYMENT_REQUESTED:
+		returnedString = "STATE_PAYMENT REQUESTED";
+		break;
+	case DRIVER_ASSIGNED:
+		returnedString = "STATE_DRIVER ASSIGNED";
+		break;
+}
+	return returnedString;
+}
---
+++ trip/StateMachine.h
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+#define NUMBER_OF_TABLE_ENTRIES 100     // event-action table의 현재 entry 갯수
+#define MAX_NUMBER_OF_ACTIONS   3       // transition 시 실행되는 최대 action 갯수
+
+using namespace std;
+
+
+// State 열거형 정의
+typedef enum {
+    READY,
+    TRIP_REQUESTED,
+    PAYMENT_REQUESTED,
+    DRIVER_ASSIGNED
+} State;
+
+
+// Event 열거형 정의
+typedef enum {
+    REQUEST_TRIP,
+    REQUEST_PAYMENT,
+    PAYMENT_FAILED,
+    PAYMENT_SUCCEEDED,
+    TRIP_COMPLETED
+} Event;
+
+
+// 음료 자동판매기를 위한 StateMachine 클래스 정의
+class StateMachine
+{
+private:
+    State curState;     // 현재 상태
+
+    // event-action table structure 정의
+    typedef struct {
+        State curState;             // 현재 상태
+        Event event;                // 사용자로부터 발생된 이벤트
+        void (StateMachine::*action)();   // action 함수 포인터의 배열
+        State nextState;            // transition 후에 변경될 다음 상태
+    } EventActionTable;
+       
+    // vending machine에서 사용할 event-action table 초기화
+    EventActionTable table[NUMBER_OF_TABLE_ENTRIES] {
+        {READY,	REQUEST_TRIP, {&StateMachine::tripRequested}, TRIP_REQUESTED},
+        {TRIP_REQUESTED, REQUEST_PAYMENT, {&StateMachine::requestPayment}, PAYMENT_REQUESTED},
+        {PAYMENT_REQUESTED, PAYMENT_FAILED, {&StateMachine::requestPayment}, TRIP_REQUESTED},
+        {PAYMENT_REQUESTED, PAYMENT_SUCCEEDED, {&StateMachine::paymentSucceeded}, DRIVER_ASSIGNED},
+        {DRIVER_ASSIGNED, TRIP_COMPLETED, {&StateMachine::tripCompleted}, READY},
+
+    };
+
+    void tripRequested();    
+    void paymentFailed();
+    void requestPayment();
+    void paymentSucceeded();
+    void tripCompleted();
+
+    Event getNextEvent();       // 사용자로부터 다음 이벤트를 입력받음
+
+    string getCurrentStateString();     // 현재 상태에 해당하는 문자열을 생성해서 반환함
+
+
+public:
+    void run();               // state machine 실행 
+};
+
+
+
+
+
+
